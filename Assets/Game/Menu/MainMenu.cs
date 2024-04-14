99402388878c9eb9b951fe9f44215c6e4515af36
Luca Schifano <luca.schifano005@gmail.com>
2024-04-14 09:56:14 +0200

    Main Menu

---
+++ Assets/Game/Menu/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject mainMenuPrefab;
+    [SerializeField] private GameObject htpMenuPrefab;
+    public void QuitButton()
+    {
+        if (Application.isEditor) EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+        mainMenuPrefab.SetActive(false);
+        htpMenuPrefab.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        mainMenuPrefab.SetActive(true);
+        htpMenuPrefab.SetActive(false);
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
